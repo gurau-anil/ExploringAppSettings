3d6123e3e01801cf229cbc73f72b9f7d044b42ba
Anil Gurau <gurau.anil@gmail.com>
2023-11-16 14:33:51 +0545

    Add project files.

---
+++ ExploringAppSettings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExploringAppSettings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
