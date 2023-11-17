5b7bd361550682ce3e4ea04887317a58c87ce0eb
Anil Gurau <gurau.anil@gmail.com>
2023-11-17 15:31:46 +0545

    Getting Configured value and Children

--- ExploringAppSettings/Controllers/HomeController.cs
+++ ExploringAppSettings/Controllers/HomeController.cs
@@ -10,16 +10,28 @@ namespace ExploringAppSettings.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IOptions<PrintSetting> _printSetting;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger, IOptions<PrintSetting> printSetting)
+        public HomeController(ILogger<HomeController> logger, IOptions<PrintSetting> printSetting, IConfiguration configuration)
         {
             _logger = logger;
             _printSetting = printSetting;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
             PrintSetting printSetting = _printSetting.Value;
+
+            // Getting Configuration Section and Mapping it to a model
+            var pSetting = _configuration.GetSection("PrintSetting").Get<PrintSetting>();
+
+            string settingVal = _configuration.GetValue<string>("MySetting");
+
+            //Getting value of subsection
+            string sectionInfo = _configuration.GetValue<string>("Section:SubSection:Info");
+            string subSectionInfo = _configuration.GetValue<string>("Section:SubSection:Info");
+            string subSubSectionInfo = _configuration.GetValue<string>("Section:SubSection:SubSubSection:Info");
             return View();
         }
 
